ebd1500408509dbeb9af90397dd6dc6edb2376b2
ArjenR <arjen@runsink.nl>
2021-06-10 21:44:16 +0200

    Create podman-deploy.sh
    Initial commit

---
+++ podman-deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+podname=${PWD##*/}
+systemctl is-active --quiet pod-${podname} && systemctl stop pod-samba_smb01.service
+podman-compose up -d ${@}
+rm *.service
+podman generate systemd --new --files --name ${podname}
+cp *.service /etc/systemd/system && \
+systemctl daemon-reload && \
+podman-compose down && \
+systemctl start pod-${podname}
